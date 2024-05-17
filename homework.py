dfc00a49e47bf01079366e905078aaa6a9686f08
Dmitrii Kuksin <snymlik@gmail.com>
2024-05-17 19:09:21 +0700

    вот так

--- homework.py
+++ homework.py
@@ -147,7 +147,7 @@ def main() -> None:
             homeworks = api_response.get('homeworks', [])
             if not homeworks:
                 logger.debug("Новых проверок домашних работ - нет.")
-                timestamp = api_response.get('current_date', timestamp)           
+                timestamp = api_response.get('current_date', timestamp)
             status_message = parse_status(homeworks[0])
             send_message(bot, status_message)
             timestamp = api_response.get('current_date', timestamp)
